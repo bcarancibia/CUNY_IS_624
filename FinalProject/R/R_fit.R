9b8b2f14b1400f1d8e43b6acb2c872dbf82cb1b7
bcarancibia <bcarancibia@gmail.com>
2015-07-19 13:15:47 -0400

    update

---
+++ FinalProject/R/R_fit.R
@@ -0,0 +1,132 @@
+## libraries
+library("matrixcalc")
+library("caTools")
+library("randomForest")
+library("caret")
+
+setwd("/users/bcarancibia/CUNY_IS_624/FinalProject")
+
+## load data
+nba1 = read.csv("/users/bcarancibia/CUNY_IS_624/FinalProject/data/scores_team_00213.csv") # team box scores 2013-14
+
+summary(nba1)
+
+summary(nba2)
+nba2 = read.csv("/users/bcarancibia/CUNY_IS_624/FinalProject/data/scores_team_00214.csv") # team box scores 2014-15
+
+nba <- rbind(nba1,nba2)
+
+summary(nba)
+
+## add data fields
+# set opponent stats in each game:
+ngames = nrow(nba)
+for (game in seq(1,ngames/2)){
+  nba$opts[2*game-1] = nba$pts[2*game]
+  nba$opts[2*game] = nba$pts[2*game-1]
+  
+  nba$ofgm[2*game-1] = nba$fgm[2*game]
+  nba$ofgm[2*game] = nba$fgm[2*game-1]
+  
+  nba$ofga[2*game-1] = nba$fga[2*game]
+  nba$ofga[2*game] = nba$fga[2*game-1]
+  
+  nba$o3pm[2*game-1] = nba$X3pm[2*game]
+  nba$o3pm[2*game] = nba$X3pm[2*game-1]
+  
+  nba$o3pa[2*game-1] = nba$X3pa[2*game]
+  nba$o3pa[2*game] = nba$X3pa[2*game-1]
+  
+  nba$oftm[2*game-1] = nba$ftm[2*game]
+  nba$oftm[2*game] = nba$ftm[2*game-1]
+  
+  nba$ofta[2*game-1] = nba$fta[2*game]
+  nba$ofta[2*game] = nba$fta[2*game-1]
+  
+  nba$oto[2*game-1] = nba$to[2*game]
+  nba$oto[2*game] = nba$to[2*game-1]
+  
+  nba$otot[2*game-1] = nba$tot[2*game]
+  nba$otot[2*game] = nba$tot[2*game-1]
+}
+
+# add shooting percentages
+nba$fgpct=nba$fgm/nba$fga * 100 #home team
+nba$X3ppct=nba$X3pm/nba$X3pa * 100 #home team
+nba$ftpct=nba$ftm/nba$fta * 100 #home team
+nba$ofgpct=nba$ofgm/nba$ofga * 100 #away team
+nba$o3ppct=nba$o3pm/nba$o3pa * 100 #away team
+nba$oftpct=nba$oftm/nba$ofta * 100 #away team
+
+# add wins
+nba$win = nba$pts > nba$opts
+
+# add home status
+nba$ishome = (nba$tm == nba$home)
+
+summary(nba)
+str(nba)
+
+#model1 <- glm(win ~ ., data=nba, family="binomial")
+#rfImp1 <- varImp(model1, scale = FALSE)
+
+#Warning this will take a long time. Variables that are of interested are ishome, fgpct, ofpct, to, oto, tot, otot, win
+
+# keep only covariates of interest
+vars = c("ishome", "fgpct", "ofgpct", "to", "oto", "tot", "otot", "win") # 87.5%
+nbaSub = nba[,vars]
+
+# break into train/test data
+library("caTools")
+split = sample.split(nbaSub$win, SplitRatio=0.7)
+nbaTrain = subset(nbaSub, split==TRUE)
+nbaTest = subset(nbaSub, split==FALSE)
+# or use the whole data set as test data
+nbaTest = nbaSub
+
+# now predict wins for test data
+nbaLog = glm(win ~ ., data=nbaTrain, family="binomial")
+nbaPredict = predict(nbaLog, newdata=nbaTest, type="response")
+cm = table(nbaTest$win, nbaPredict>0.5)
+cm
+plot(cm, col = "blue", main="Prediction Using GLM")
+library("matrixcalc")
+matrix.trace(cm) / sum(cm)
+summary(nbaLog)
+
+#reset values
+nbaTest = nbaSub
+nbaSub = nba[,vars]
+
+split = sample.split(nbaSub$win, SplitRatio=0.7)
+nbaTrain = subset(nbaSub, split==TRUE)
+nbaTest = subset(nbaSub, split==FALSE)
+
+# decision tree
+library(rpart)
+nbaMod = rpart(win ~ ., data=nbaTrain, method="class", minbucket=50)
+library(rpart.plot)
+prp(nbaMod) # plot tree
+nbaPredict = predict(nbaMod, newdata=nbaTest, type="class")
+
+plot(nbaMod, uniform=TRUE, 
+     main="Classification Tree for NBA")
+text(nbaMod, use.n=TRUE, all=TRUE, cex=.8)
+
+summary(nbaPredict)
+
+summary(nbaMod)
+
+cm = table(nbaTest$win, nbaPredict > 0.5) # if thresholding needed to classify, e.g. log regression
+cm = table(nbaTest$win, nbaPredict)
+
+print(cm)
+plot(cm, col = "blue", main="Prediction Using Decision Tree")
+
+library("matrixcalc")
+matrix.trace(cm) / sum(cm)
+summary(nbaMod)
+
+
+
+
--- FinalProject/nba_scrap.R
+++ FinalProject/nba_scrap.R
@@ -1,3 +1,4 @@
+
 nba <- read.csv("/users/bcarancibia/CUNY_IS_624/FinalProject/nbaplayerdata.csv", header=TRUE)
 
 View(nba)
@@ -14,4 +15,6 @@ unique(nba$age)
 unique(nba$bref_team_id)
 
 #One thing to notice is that the largest amount for teams is TOT which means that is total for all teams(player played for more than one team that year)
+
+summary(nba$age)
 
